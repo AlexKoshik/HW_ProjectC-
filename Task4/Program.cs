6b13b59b991eae2af0a98c4ef3a3de96af0c568c
AlexK <kobra41@live.ru>
2023-04-21 10:10:08 +0300

    add decision task4

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -5,3 +5,26 @@
 // 2, 3, 7 -> 7
 // 44 5 78 -> 78
 // 22 3 9 -> 22
+
+
+Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+if (num1>num2)
+{
+    if (num1>num3)
+    {
+        Console.WriteLine("Первое число - наибольшее");
+    }
+}
+if (num2>num3)
+{
+    Console.WriteLine("Второе число - наибольшее");
+}
+else
+{
+    Console.WriteLine("Третье число - наибольшее");
+}
