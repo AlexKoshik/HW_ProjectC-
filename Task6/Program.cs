83e2f8ae97605ce5d785a8b33eb14bc35f71382d
Alex <kobra41@live.rum>
2023-04-23 17:00:39 +0300

    add decision task6

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -5,3 +5,11 @@
 // 4 -> да
 // -3 -> нет
 // 7 -> нет
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num % 2 != 0 ) Console.WriteLine("нет");
+else
+{
+    Console.WriteLine("да");
+} 
