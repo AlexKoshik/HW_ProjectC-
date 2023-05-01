ba3bb2cab0eb7c6cccbfe525b3ff1c3f98ea8f0b
Alex <kobra41@live.rum>
2023-05-01 14:14:02 +0300

    add decision task21

--- Task21/Program.cs
+++ Task21/Program.cs
@@ -6,3 +6,17 @@
 
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
+Console.WriteLine("Введите координату x точки а : ");
+double x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату y точки а : ");
+double y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату z точки а : ");
+double z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату x точки b : ");
+double x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату y точки b : ");
+double y2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату z точки b : ");
+double z2 = Convert.ToInt32(Console.ReadLine());
+double distance = Math.Sqrt(Math.Pow((x2-x1), 2)+Math.Pow((y2-y1), 2)+ Math.Pow((z2-z1), 2));
+Console.WriteLine($"Расстояние между a и b = {Math.Round(distance, 3)}");
