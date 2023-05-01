5edeacfc0f784a3cac67083cbdccad6fe24a4e38
Alex <kobra41@live.rum>
2023-05-01 13:52:52 +0300

    add decision task19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -7,3 +7,29 @@
 // 12821 -> да
 
 // 23432 -> да
+
+Console.WriteLine("Введите  пятизначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 10 == num / 10000)
+{
+    while (num >= 99)
+    {
+        num /= 10;
+    }
+    num = num % 10;
+    int a = num;
+    while (num >= 9999)
+    {
+        num /= 10;
+    }
+    num = num % 10;
+    int b = num;
+    if (a==b)
+    {
+        Console.WriteLine(" да ");
+    }
+}
+else
+{
+    Console.WriteLine(" нет ");
+}
