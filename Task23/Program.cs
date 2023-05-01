9cc5b7f0e18d75730b59e2b49499aac7eee61aaf
Alex <kobra41@live.rum>
2023-05-01 14:23:18 +0300

    add decision task23

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -4,3 +4,15 @@
 
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число: ");
+double a = Convert.ToInt32(Console.ReadLine());
+double i = 1;
+double result = 1;
+Console.Write($"{a}=> ");
+while (i <= a)
+{
+    result = Math.Pow(i, 3);
+    Console.Write($"{result}, ");
+    i++;
+}
