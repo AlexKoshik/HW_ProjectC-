1d7ad3ebcbf2a815c78bbd2ff172104a9b1719dd
Alex <kobra41@live.rum>
2023-05-21 16:36:26 +0300

    add task25 not

--- task25/Program.cs
+++ task25/Program.cs
@@ -5,3 +5,20 @@
 
 // 2, 4 -> 16
 
+Console.WriteLine("Введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int num = ExpNum();
+int ExpNum()
+{
+    int result=a;
+    int count = 1;
+    for (int i = a; count <= b; count++)
+    {   
+        result = result * result;
+    }
+    return result;
+}
+Console.WriteLine(num);
