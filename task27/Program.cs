dd3df9f47335247e741ba6f06b5d72fb073082ed
AlexK <kobra41@live.ru>
2023-05-22 17:08:09 +0300

    add task27 wow

--- task27/Program.cs
+++ task27/Program.cs
@@ -7,3 +7,17 @@
 // 82 -> 10
 
 // 9012 -> 12
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int SumNum()
+{   
+    int sum = 0; 
+    while (num>0)
+    {
+       sum = sum + num % 10;
+       num = num / 10; 
+    } return sum;
+}
+Console.WriteLine(SumNum());
+
