e8bf032cb9e6e907eee1468923fd6376f97d8323
AlexK <kobra41@live.ru>
2023-05-23 09:16:11 +0300

    add task27 desision

--- task29/Program.cs
+++ task29/Program.cs
@@ -5,3 +5,21 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 
 // 6, 1, 33 -> [6, 1, 33]
+
+void RandomArr(int[] arr)
+{
+    for (int i = 0; i < 8; i++)
+    {
+        arr[i] = new Random().Next(-100, 100);
+    }
+}
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < 8; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+int[] array = new int[8];
+RandomArr(array);
+PrintArray(array);
