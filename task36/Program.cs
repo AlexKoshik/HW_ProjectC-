484e1c2aeab4b5d3b60ff45a86b9fc108585d574
AlexK <kobra41@live.ru>
2023-05-23 11:04:22 +0300

    add task36 decision

--- task36/Program.cs
+++ task36/Program.cs
@@ -6,3 +6,32 @@
 // [3, 7, 23, 12] -> 19
 
 // [-4, -6, 89, 6] -> 0
+
+void RandomArr(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(-99, 99);
+    }
+}
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+void SumElOdPos(int[] arr)
+{
+    for (int i = 1; i < arr.Length ; i++)
+    {
+        int sum = arr[i] + arr[i + 2];
+        Console.WriteLine($"{sum} ");
+    }
+}
+
+int[] array = new int[4];
+RandomArr(array);
+PrintArray(array);
+SumElOdPos(array);
