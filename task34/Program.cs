e8d9730ade116c4e280541dc5d1ae2127049f579
AlexK <kobra41@live.ru>
2023-05-23 10:20:30 +0300

    add task34 decision

--- task34/Program.cs
+++ task34/Program.cs
@@ -4,3 +4,35 @@
 //   покажет количество чётных чисел в массиве.
 
 // [345, 897, 568, 234] -> 2
+
+void RandomArr(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(100, 999);
+    }
+}
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+void CountSearchEvenNum(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            count = count + 1;
+        }
+    }
+    Console.WriteLine(count);
+}
+int[] array = new int[6];
+RandomArr(array);
+PrintArray(array);
+CountSearchEvenNum(array);
