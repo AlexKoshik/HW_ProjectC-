ea4460d75a329acc768b49cf7f5e9965dd7cae5c
AlexK <kobra41@live.ru>
2023-05-25 15:54:46 +0300

    add task38 decision

--- task38/Program.cs
+++ task38/Program.cs
@@ -4,3 +4,47 @@
 // минимальным элементов массива.
 
 // [3 7 22 2 78] -> 76
+
+Console.WriteLine("Введите длину массива: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+
+void RandomArr(double[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(0, 99);
+    }
+}
+void PrintArray(double[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+void DifNum(double[] arr)
+{
+
+    double max = double.MinValue;
+    double min = double.MaxValue;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+        if (arr[i] < min)
+        {
+            min = arr[i];
+        }
+    }
+    Console.Write(max - min);
+}
+
+
+double[] array = new double[a];
+RandomArr(array);
+PrintArray(array);
+DifNum(array);
