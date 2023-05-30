6e3e48d06b593d45d62479caae49ebbccde7d904
AlexK <kobra41@live.ru>
2023-05-30 11:03:17 +0300

    add task41 decision

--- task41/Program.cs
+++ task41/Program.cs
@@ -4,3 +4,48 @@
 // 0, 7, 8, -2, -2 -> 2
 
 // 1, -7, 567, 89, 223-> 3
+
+int ReadInputNumbers(string message) //читает число с консоли, вводимое пользователем
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+
+int[] InputArray(int length)
+{
+    int[] array = new int[length]; 
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = ReadInputNumbers($"Введите {i + 1}-й элемент ");
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"a[{i}] = {array[i]}");
+    }
+}
+
+int CountPositiveNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int length = ReadInputNumbers($"Введите количество элементов ");
+int[] array;
+array = InputArray(length);
+PrintArray(array);
+Console.WriteLine($"Количество чисел больше нуля - {CountPositiveNumbers(array)}");
