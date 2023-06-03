18b5c2d313fe413b8bea0d53dce6fc4022b1137f
Alex <kobra41@live.rum>
2023-06-03 16:13:01 +0300

    add task52 decision

--- task52/Program.cs
+++ task52/Program.cs
@@ -6,3 +6,54 @@
 // 5 9 2 3
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int InputNumber()
+{
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+void CraeteArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ArithmeticsMean(int[,] arr)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            sum = sum + arr[i, j];
+        }
+        double c = Math.Round(sum / arr.GetLength(0), 1);
+        Console.Write($"{c} ");
+    }
+}
+
+Console.Write("Введите количество строк массива: ");
+int row = InputNumber();
+Console.Write("Введите количество столбцов массива: ");
+int colums = InputNumber();
+int[,] array = new int[row, colums];
+CraeteArray(array);
+PrintArray(array);
+ArithmeticsMean(array);
