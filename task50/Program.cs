58beb02a12137f47bf4ffb29bc3e7e8b1fb781e3
Alex <kobra41@live.rum>
2023-06-03 13:31:36 +0300

    add task50 decision

--- task50/Program.cs
+++ task50/Program.cs
@@ -13,3 +13,55 @@
 // 8 4 2 4
 
 // 17 -> такого числа в массиве нет
+
+// int InputNumber()
+// {
+//     int num = new Random().Next(5, 5);
+//     return num;
+// }
+
+Console.WriteLine("Введите строку: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите столбец: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+void CraeteArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FindNumber(int[,] arr)
+{
+
+    if (n < 0 | n > arr.GetLength(0) - 1 | m < 0 | m > arr.GetLength(1) - 1)
+    {
+        Console.WriteLine("Элемент не существует  ");
+    }
+    else
+    {
+        Console.WriteLine("Значение элемента массива = {0} ", arr[n, m]);
+    }
+}
+int row = 5, colums = 6;
+int[,] array = new int[row, colums];
+CraeteArray(array);
+Console.WriteLine("Исходный массив: ");
+PrintArray(array);
+FindNumber(array);
