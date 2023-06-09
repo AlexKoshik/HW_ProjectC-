45e22da959dca60697c8355796aaa15a3fbd22f7
AlexK <kobra41@live.ru>
2023-06-09 14:44:41 +0300

    add task66 decision

--- task66/Program.cs
+++ task66/Program.cs
@@ -4,3 +4,29 @@
 
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите натуральное цисло M: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральное цисло N: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+
+int Sum(int m, int n)
+{
+    int res = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        res = m + Sum(m, n);
+        return res;
+    }
+}
+void PrintSum(int m, int n)
+{
+    Console.Write($"Сумма натуральных чисел от M до N равна: {Sum(m - 1, n)}");
+}
+
+
+PrintSum(x, y);
