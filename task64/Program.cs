37becee118c490b474ad4247fe731b8e051a4e56
AlexK <kobra41@live.ru>
2023-06-09 12:05:33 +0300

    add task64 decision

--- task64/Program.cs
+++ task64/Program.cs
@@ -4,3 +4,26 @@
 
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.WriteLine("Введите натуральное число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+if (x < 1)
+{
+Console.WriteLine("Введено неверное число, перезапустите программу и повторите ввод!");
+}
+else
+{
+    Massive(1, x);
+}
+
+void Massive(int start, int end)
+{
+if(end == start) Console.Write($"{end}, ");
+else
+{
+Massive(start + 1, end);
+Console.Write($"{start}, ");
+}
+}
+
